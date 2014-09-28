67527f84c16de9b7964b20b5b4bde7afcbab0914
Samuel <Samuel_ipad2@hotmail.com>
2014-09-28 20:11:15 +1000

    Updated Main.php done a little bit of work

--- src/mightysun99/TrippyTime/Main.php
+++ src/mightysun99/TrippyTime/Main.php
@@ -1,1 +1,9 @@
 <?php //Start of a php code
+
+namespace mightysun99\TrippyTime; //namespace of the code
+
+use pocketmine\command\Command; //Uses Pocketmine SRC Command
+use pocketmine\command\CommandSender; //Uses Pocketmine SRC Command
+use pocketmine\command\ConsoleCommandSender; //Uses Pocketmine SRC Command
+
+class Main extends PluginBase{ //class
