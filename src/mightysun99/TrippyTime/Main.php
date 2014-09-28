ea54906e92c64a155c5d6a2087d9fc8f9bb6b342
Samuel <Samuel_ipad2@hotmail.com>
2014-09-28 20:15:26 +1000

    :D

--- src/mightysun99/TrippyTime/Main.php
+++ src/mightysun99/TrippyTime/Main.php
@@ -7,3 +7,15 @@ use pocketmine\command\CommandSender; //Uses Pocketmine SRC Command
 use pocketmine\command\ConsoleCommandSender; //Uses Pocketmine SRC Command
 
 class Main extends PluginBase{ //class
+
+    public function onEnable(){  //function when plugin enable
+        $this->getLogger()->info("TrippyTime has been enabled."); //sends message
+    } 
+    
+    public function onDisable(){ //function when plugin disable
+        $this->getLogger()->info("TrippyTime has been disabled."); //sends message
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){ //adds a functions
+      case "TrippyTime": //adds the command line
+        
